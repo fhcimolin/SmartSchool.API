1d23c59cd15b3c16c6477ced45572e36a6411411
DEPSSC\flavio.simolin <flavio@deps.com.br>
2021-09-28 12:18:30 -0300

    Finalizado controller aluno

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SmartSchool.API.Data;
 using SmartSchool.API.Models;
 
@@ -39,24 +40,57 @@ namespace SmartSchool.API.Controllers
         [HttpPost]
         public IActionResult Post(Aluno aluno)
         {
+            _context.Add(aluno);
+            _context.SaveChanges();
+
             return Ok(aluno);
         }
 
         [HttpPut("{id}")]
         public IActionResult Put(int id, Aluno aluno)
         {
+            var alu = _context.Alunos.AsNoTracking().FirstOrDefault(x => x.Id == id);
+            
+            if(alu == null)
+            {
+                return BadRequest("Aluno não encontrado.");
+            }
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+
             return Ok(aluno);
         }
 
         [HttpPatch("{id}")]
         public IActionResult Patch(int id, Aluno aluno)
         {
+            var _aluno = _context.Alunos.FirstOrDefault(x => x.Id == id);
+
+            if(_aluno == null)
+            {
+                return BadRequest("Aluno não encontrado.");
+            }
+
+            _context.Update(aluno);
+            _context.SaveChanges();
+
             return Ok(aluno);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult Put(int id)
+        public IActionResult Delete(int id)
         {
+            var aluno = _context.Alunos.FirstOrDefault(x => x.Id == id);
+
+            if(aluno == null)
+            {
+                return BadRequest("Aluno não encontrado.");
+            }
+
+            _context.Remove(aluno);
+            _context.SaveChanges();
+
             return Ok();
         }
     }

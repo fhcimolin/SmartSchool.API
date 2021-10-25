b2d6a7c3932bf1905a93ca078e5321a07b38df83
DEPSSC\flavio.simolin <flavio@deps.com.br>
2021-10-25 12:21:02 -0300

    Expansão de modelos

--- Models/Disciplina.cs
+++ Models/Disciplina.cs
@@ -18,8 +18,14 @@ namespace SmartSchool.API.Models
 
         public int Id { get; set; }
         public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
         public int ProfessorId { get; set; }
         public Professor Professor { get; set; }
+        public int? PrerequisitoId { get; set; } = null;
+        public Disciplina Prerequisito { get; set; }
+        public int CursoId { get; set; }
+        
+        public Curso Curso { get; set; }
 
         public IEnumerable<AlunoDisciplina> AlunosDisciplinas { get; set; }
     }
